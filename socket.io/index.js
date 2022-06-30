8123b9891918aeeb2efb5c8da35172bec419a084
Seonmi Jung <devkatej927@gmail.com>
2022-06-30 17:57:58 +0900

    [socket.io] feat: finished Get started

---
+++ socket.io/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
+  })
+})
+
+server.listen(9000, () => {
+  console.log('listening on *:9000')
+})
